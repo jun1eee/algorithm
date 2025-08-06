7b769c69baf456ccf112d41ff5e61a268c1eec70
jun1eee <162537692+jun1eee@users.noreply.github.com>
2025-08-06 19:11:39 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 250806/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
+++ 250806/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -11,6 +11,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printStars(5);
+        printStars(n);
     }
 }
