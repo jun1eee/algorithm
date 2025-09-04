f2ad5f8c556851675f79979d9734007c11348525
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-04 21:07:22 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,78 +1,57 @@
 import java.io.*;
 import java.util.*;
 
-class Pair {
-	int x, y;
-
-	public Pair(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-
 public class Main {
-
-	static int N, M;
-	static int[][] grid;
-	static int[][] dist;
-	static boolean[][] visited;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
+	private static int N, M;
+	private static int[][] map;
+	private static boolean[][] visited;
+	private static int[] dr = { -1, 1, 0, 0 };
+	private static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
 
-		grid = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < M; j++) {
-				grid[i][j] = str.charAt(j) - '0';
+				map[i][j] = str.charAt(j) - '0';
 			}
 		}
+
 		visited = new boolean[N][M];
-		dist = new int[N][M];
-		System.out.println(bfs());
+		bfs();
+
+		System.out.println(map[N - 1][M - 1]);
 	}
 
-	static int bfs() {
-		Queue<Pair> q = new ArrayDeque<>();		
+	private static void bfs() {
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] { 0, 0 });
 		visited[0][0] = true;
-		dist[0][0] = 1;
-		
-		q.offer(new Pair(0, 0));
-		
+
 		while (!q.isEmpty()) {
-			Pair cur = q.poll();
-			int x = cur.x;
-			int y = cur.y;
-
-			if (x == N - 1 && y == M - 1) {
-				return dist[x][y];
-			}
-
+			int[] cur = q.poll();
+			int r = cur[0];
+			int c = cur[1];
+			if (r == N - 1 && c == M - 1)
+				return;
 			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
+				int nr = r + dr[i];
+				int nc = c + dc[i];
 
-				if (inRange(nx, ny) && !visited[nx][ny] && grid[nx][ny] == 1) {
-					visited[nx][ny] = true;
-					dist[nx][ny] = dist[x][y] + 1;
-					q.offer(new Pair(nx, ny));
-				}
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M)
+					continue;
+				if (map[nr][nc] == 0 || visited[nr][nc])
+					continue;
+				visited[nr][nc] = true;
+				map[nr][nc] = map[r][c] + 1;
+				q.offer(new int[] { nr, nc });
 			}
 		}
-		return -1;
 	}
-
-	
-	static boolean inRange(int x, int y) {
-		return 0 <= x && x < N && 0 <= y && y < M;
-	}
-
 }
-
-
-
