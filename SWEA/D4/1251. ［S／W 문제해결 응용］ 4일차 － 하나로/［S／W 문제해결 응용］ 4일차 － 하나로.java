fd903eb88ac63d597006c53f97f4d6bf82628bc4
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-09 17:15:33 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 666 ms, Memory: 96,352 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -35,7 +35,6 @@ public class Solution {
     }
     
     public static void main(String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         
