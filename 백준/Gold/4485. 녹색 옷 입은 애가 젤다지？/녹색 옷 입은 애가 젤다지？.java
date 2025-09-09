2ed2b95110c7aaf917db3f5ba0ddbd7689e9bb5f
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-09 22:10:10 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 196 ms, Memory: 19672 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,75 +2,80 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static class Node implements Comparable<Node> {
-		int i, j, weight;
+    public static int n;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+    public static int[][] map;
+    public static int[][] distance;
+    public static final int INF = (int) 1e9;
 
-		Node(int i, int j, int weight) {
-			this.i = i;
-			this.j = j;
-			this.weight = weight;
-		}
+    public static class Node implements Comparable<Node> {
+        int x;
+        int y;
+        int distance;
 
-		@Override
-		public int compareTo(Node o) {
-			return this.weight - o.weight;
-		}
-	}
+        public Node(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
 
-	private static final int INF = 100_000_000;
-	private static int N;
-	private static int[][] map;
-	private static int[][] dist;
-	private static int[] dr = { -1, 1, 0, 0 };
-	private static int[] dc = { 0, 0, -1, 1 };
-	
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.distance, o.distance);
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int cnt = 1;
-		while (true) {
-			N = Integer.parseInt(br.readLine());
-			if (N == 0) break;
-			map = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			dist = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				Arrays.fill(dist[i], INF);
-			}
-			int result = dijkstra();
-			sb.append("Problem").append(" ").append(cnt++).append(": ").append(result).append("\n");
-			
-		}
-		System.out.println(sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int cnt = 1;
 
-	private static int dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		dist[0][0] = map[0][0];
-		pq.offer(new Node(0, 0, dist[0][0]));
-		
-		while (!pq.isEmpty()) {
-			Node curNode = pq.poll();
-			int curI = curNode.i;
-			int curJ = curNode.j;
-			int curWeight = curNode.weight;
-			if (curI == N-1 && curJ == N-1) return curWeight;
-			for (int i = 0; i < 4; i++) {
-				int nextI = curI + dr[i];
-				int nextJ = curJ + dc[i];
-				if (nextI < 0 || nextJ < 0 || nextI >= N || nextJ >= N) continue;
-				if (dist[nextI][nextJ] > dist[curI][curJ] + map[nextI][nextJ]) {
-					dist[nextI][nextJ] = dist[curI][curJ] + map[nextI][nextJ];
-					pq.add(new Node(nextI, nextJ, dist[nextI][nextJ]));
-				}
-			}
-		}
-		return -1;
-	}
+        while (true) {
+            n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+
+            map = new int[n][n];
+            distance = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    distance[i][j] = INF;
+                }
+            }
+
+            dijkstra();
+            sb.append("Problem ").append(cnt++).append(": ").append(distance[n - 1][n - 1]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        distance[0][0] = map[0][0];
+        pq.offer(new Node(0, 0, distance[0][0]));
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+
+            // 구버전 상태 스킵
+//            if (node.distance > distance[node.x][node.y]) continue;
+
+            for (int d = 0; d < 4; d++) {
+                int nx = node.x + dx[d];
+                int ny = node.y + dy[d];
+
+                if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                    int cost = node.distance + map[nx][ny];
+                    if (cost < distance[nx][ny]) {
+                        distance[nx][ny] = cost;
+                        pq.offer(new Node(nx, ny, cost));
+                    }
+                }
+            }
+        }
+    }
 }
