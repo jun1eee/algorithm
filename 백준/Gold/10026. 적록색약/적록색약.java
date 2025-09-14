4d8d0aef0481405d42c78880b0f9ac96547c5cf8
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-14 22:52:33 +0900

    [Gold V] Title: 적록색약, Time: 132 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,106 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static char[][] map;
-	static boolean[][] visited;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+    static int N;
+    static char[][] map;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-	public static class Pair {
-		int r, c;
-		char color;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
 
-		Pair(int r, int c, char color) {
-			this.r = r;
-			this.c = c;
-			this.color = color;
-		}
-	}
+        // 1) 일반 시야
+        int normal = countRegions(map);
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new char[N][N];
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			map[i] = str.toCharArray();
-		}
-		int cnt = 0;
-		visited = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					cnt++;
-					bfs(i, j);
-				}
-			}
-		}
-		int cntRGB = 0;
-		visited = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					cntRGB++;
-					bfsRGB(i, j);
-				}
-			}
-		}
-		System.out.println(cnt + " " + cntRGB);
-	}
+        // 2) 적록색약: G -> R 로 맵을 변환한 뒤 같은 BFS 재사용
+        char[][] cbMap = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                char ch = map[i][j];
+                cbMap[i][j] = (ch == 'G') ? 'R' : ch;
+            }
+        }
+        int colorBlind = countRegions(cbMap);
 
-	private static void bfs(int i, int j) {
-		Queue<Pair> q = new ArrayDeque<>();
-		visited[i][j] = true;
-		q.offer(new Pair(i, j, map[i][j]));
-		while (!q.isEmpty()) {
-			Pair cur = q.poll();
-			int r = cur.r;
-			int c = cur.c;
-			for (int k = 0; k < 4; k++) {
-				int nr = r + dr[k];
-				int nc = c + dc[k];
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N)
-					continue;
-				if (map[nr][nc] == cur.color && !visited[nr][nc]) {
-					visited[nr][nc] = true;
-					q.offer(new Pair(nr,nc,map[nr][nc]));
-				}
-			}
-		}
-	}
-	private static void bfsRGB(int i, int j) {
-		Queue<Pair> q = new ArrayDeque<>();
-		visited[i][j] = true;
-		q.offer(new Pair(i, j, map[i][j]));
-		while (!q.isEmpty()) {
-			Pair cur = q.poll();
-			int r = cur.r;
-			int c = cur.c;
-			if (cur.color == 'B') {
-				for (int k = 0; k < 4; k++) {
-					int nr = r + dr[k];
-					int nc = c + dc[k];
-					if (nr < 0 || nc < 0 || nr >= N || nc >= N)
-						continue;
-					if (map[nr][nc] == cur.color && !visited[nr][nc]) {
-						visited[nr][nc] = true;
-						q.offer(new Pair(nr,nc,map[nr][nc]));
-					}
-				}
-			}
-			else {
-				for (int k = 0; k < 4; k++) {
-					int nr = r + dr[k];
-					int nc = c + dc[k];
-					if (nr < 0 || nc < 0 || nr >= N || nc >= N)
-						continue;
-					if (map[nr][nc] != 'B' && !visited[nr][nc]) {
-						visited[nr][nc] = true;
-						q.offer(new Pair(nr,nc,map[nr][nc]));
-					}
-				}
-			}
-		}
-	}
+        System.out.println(normal + " " + colorBlind);
+    }
+
+    // 같은 색 연결요소 개수 세기 (BFS, primitive queue)
+    static int countRegions(char[][] g) {
+        boolean[][] visited = new boolean[N][N];
+        int regions = 0;
+
+        // 고정 최대 크기 큐 (객체 생성 없이 빠름)
+        int maxQ = N * N;
+        int[] qr = new int[maxQ];
+        int[] qc = new int[maxQ];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i][j]) continue;
+                regions++;
+
+                char color = g[i][j];
+                int head = 0, tail = 0;
+                visited[i][j] = true;
+                qr[tail] = i; qc[tail] = j; tail++;
+
+                while (head < tail) {
+                    int r = qr[head], c = qc[head]; head++;
+                    for (int d = 0; d < 4; d++) {
+                        int nr = r + dr[d], nc = c + dc[d];
+                        if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+                        if (!visited[nr][nc] && g[nr][nc] == color) {
+                            visited[nr][nc] = true;
+                            qr[tail] = nr; qc[tail] = nc; tail++;
+                        }
+                    }
+                }
+            }
+        }
+        return regions;
+    }
 }
