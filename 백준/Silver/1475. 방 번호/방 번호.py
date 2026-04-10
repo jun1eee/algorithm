06d57907652f6153012021fe25e29ea3eeb9f32e
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-04-10 19:13:39 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,11 +1,12 @@
-numbers = list(map(int,input()))
-count = [0]*10
-for n in numbers:
-    count[n] += 1
-max_num = -1
+import sys
+input = sys.stdin.readline
+nums = list(map(int,input().rstrip()))
+cnt = [0] * 10
+for n in nums:
+    cnt[n] += 1
+max_cnt = -1
 for i in range(10):
     if i == 6 or i == 9:
         continue
-    max_num = max(max_num, count[i])
-max_num = max(max_num, ((count[6] + count[9] + 1)//2))
-print(max_num)
+    max_cnt = max(max_cnt, cnt[i])
+print(max(max_cnt, (cnt[6] + cnt[9] + 1)//2))
